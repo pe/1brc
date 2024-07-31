4a81ee2d8e139517d0805018bb487d211726911d
Philippe Eberli <p.eberli@gmx.ch>
2024-07-31 22:55:45 +0200

    Initial cleanup

--- src/main/java/dev/morling/onebrc/CalculateAverage_baseline.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_baseline.java
@@ -15,27 +15,56 @@
  */
 package dev.morling.onebrc;
 
-import static java.util.stream.Collectors.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class CalculateAverage_baseline {
+    private static final Path FILE = Paths.get("./measurements.txt");
 
-    private static final String FILE = "./measurements.txt";
-
-    private static record Measurement(String station, double value) {
-        private Measurement(String[] parts) {
-            this(parts[0], Double.parseDouble(parts[1]));
-        }
+    public static void main(String[] args) throws IOException {
+        Map<String, Result> measurements = Files.lines(FILE)
+                .map(line -> {
+                    String[] split = line.split(";");
+                    return new Measurement(split[0], Double.parseDouble(split[1]));
+                })
+                .collect(Collectors.groupingBy(Measurement::station, Collector.of(
+                        AggregatedMeasurements::new,
+                        (agg, measurement) -> {
+                            agg.min = Math.min(agg.min, measurement.value);
+                            agg.max = Math.max(agg.max, measurement.value);
+                            agg.sum += measurement.value;
+                            agg.count++;
+                        },
+                        (agg1, agg2) -> {
+                            var agg = new AggregatedMeasurements();
+                            agg.min = Math.min(agg1.min, agg2.min);
+                            agg.max = Math.max(agg1.max, agg2.max);
+                            agg.sum = agg1.sum + agg2.sum;
+                            agg.count = agg1.count + agg2.count;
+                            return agg;
+                        },
+                        agg -> new Result(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max))));
+        Map<String, Result> sorted = new TreeMap<>(measurements);
+        System.out.println(sorted);
     }
 
-    private static record ResultRow(double min, double mean, double max) {
+    private record Measurement(String station, double value) {
+    }
 
+    private static class AggregatedMeasurements {
+        private double min = Double.POSITIVE_INFINITY;
+        private double max = Double.NEGATIVE_INFINITY;
+        private double sum;
+        private long count;
+    }
+
+    private record Result(double min, double mean, double max) {
         public String toString() {
             return round(min) + "/" + round(mean) + "/" + round(max);
         }
@@ -43,50 +72,5 @@ public class CalculateAverage_baseline {
         private double round(double value) {
             return Math.round(value * 10.0) / 10.0;
         }
-    };
-
-    private static class MeasurementAggregator {
-        private double min = Double.POSITIVE_INFINITY;
-        private double max = Double.NEGATIVE_INFINITY;
-        private double sum;
-        private long count;
-    }
-
-    public static void main(String[] args) throws IOException {
-        // Map<String, Double> measurements1 = Files.lines(Paths.get(FILE))
-        // .map(l -> l.split(";"))
-        // .collect(groupingBy(m -> m[0], averagingDouble(m -> Double.parseDouble(m[1]))));
-        //
-        // measurements1 = new TreeMap<>(measurements1.entrySet()
-        // .stream()
-        // .collect(toMap(e -> e.getKey(), e -> Math.round(e.getValue() * 10.0) / 10.0)));
-        // System.out.println(measurements1);
-
-        Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-                MeasurementAggregator::new,
-                (a, m) -> {
-                    a.min = Math.min(a.min, m.value);
-                    a.max = Math.max(a.max, m.value);
-                    a.sum += m.value;
-                    a.count++;
-                },
-                (agg1, agg2) -> {
-                    var res = new MeasurementAggregator();
-                    res.min = Math.min(agg1.min, agg2.min);
-                    res.max = Math.max(agg1.max, agg2.max);
-                    res.sum = agg1.sum + agg2.sum;
-                    res.count = agg1.count + agg2.count;
-
-                    return res;
-                },
-                agg -> {
-                    return new ResultRow(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max);
-                });
-
-        Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(FILE))
-                .map(l -> new Measurement(l.split(";")))
-                .collect(groupingBy(m -> m.station(), collector)));
-
-        System.out.println(measurements);
     }
 }
