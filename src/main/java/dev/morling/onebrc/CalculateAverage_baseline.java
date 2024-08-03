c1b9aa631b5850da7777f8557918d97933f94d54
Philippe Eberli <p.eberli@gmx.ch>
2024-08-03 22:22:21 +0200

    Read chunked

--- src/main/java/dev/morling/onebrc/CalculateAverage_baseline.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_baseline.java
@@ -16,20 +16,28 @@
 package dev.morling.onebrc;
 
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.TreeMap;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class CalculateAverage_baseline {
     private static final Path FILE = Paths.get("./measurements.txt");
 
     public static void main(String[] args) throws IOException {
-        Map<String, Result> measurements = Files.lines(FILE)
+        var channel = FileChannel.open(FILE, StandardOpenOption.READ);
+        Map<String, Result> measurements = splitIntoChunks(channel)
                 .parallel()
+                .flatMap(CalculateAverage_baseline::readLines)
                 .map(line -> {
                     int separatorPos = line.indexOf(";");
                     return new Measurement(line.substring(0, separatorPos), Double.parseDouble(line.substring(separatorPos + 1)));
@@ -53,6 +61,62 @@ public class CalculateAverage_baseline {
                         agg -> new Result(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max))));
         Map<String, Result> sorted = new TreeMap<>(measurements);
         System.out.println(sorted);
+    }
+
+    private static Stream<ByteBuffer> splitIntoChunks(final FileChannel fileChannel) throws IOException {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(new Iterator<>() {
+            private static final long CHUNK_SIZE = 10L * 1024 * 1024;
+
+            private final long size = fileChannel.size();
+            private long position = 0;
+
+            @Override
+            public boolean hasNext() {
+                return position < size;
+            }
+
+            @Override
+            public ByteBuffer next() {
+                try {
+                    var byteBuffer = fileChannel.map(MapMode.READ_ONLY, position, Math.min(CHUNK_SIZE, size - position));
+
+                    // don't split the data in the middle of lines
+                    // find the closest previous newline
+                    int realEnd = byteBuffer.limit() - 1;
+                    while (byteBuffer.get(realEnd) != '\n') {
+                        realEnd--;
+                    }
+                    realEnd++;
+
+                    position += realEnd;
+                    return byteBuffer.limit(realEnd);
+                } catch (IOException ex) {
+                    throw new UncheckedIOException(ex);
+                }
+            }
+        }, Spliterator.IMMUTABLE), true);
+
+    }
+
+    private static Stream<String> readLines(ByteBuffer byteBuffer) {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(new Iterator<>() {
+
+            @Override
+            public boolean hasNext() {
+                return byteBuffer.hasRemaining();
+            }
+
+            @Override
+            public String next() {
+                ByteBuffer string = byteBuffer.slice();
+                int length = 0;
+                while (byteBuffer.hasRemaining() && byteBuffer.get() != '\n') {
+                    length++;
+                }
+                string.limit(length);
+                return StandardCharsets.UTF_8.decode(string).toString();
+            }
+        }, Spliterator.IMMUTABLE), true);
     }
 
     private record Measurement(String station, double value) {
